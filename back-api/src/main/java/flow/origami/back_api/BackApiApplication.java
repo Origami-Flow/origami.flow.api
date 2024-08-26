a69ddb7992810ff15ef4b792f4c5e54e007d354c
davifeitosa0 <davi.rodrigues@sptech.school>
2024-08-26 19:45:56 -0300

    FEAT - Commit inicial do back com spring

---
+++ back-api/src/main/java/flow/origami/back_api/BackApiApplication.java
@@ -0,0 +1,13 @@
+package flow.origami.back_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackApiApplication.class, args);
+	}
+
+}
---
+++ back-api/src/test/java/flow/origami/back_api/BackApiApplicationTests.java
@@ -0,0 +1,13 @@
+package flow.origami.back_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
