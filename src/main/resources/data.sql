b8adf733d26cafcc0246af3197fd951d23fe1884
HeloSalgado <heloisa.salgado@sptech.school>
2024-11-18 17:00:03 -0300

    adição do database-platform e mudança no insert

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,9 @@
 INSERT INTO cliente (
-    id, nome, email, senha, token,
+    nome, email, senha, token,
     data_nascimento, telefone, genero, tipo_cabelo,
     comprimento_cabelo, cor_cabelo, progressiva,
     primeira_tranca, ocupacao, endereco_id
 ) VALUES (
-             1,
           'João Silva',
           'joao.silva@example.com',
           'senha123',
@@ -15,16 +14,15 @@ INSERT INTO cliente (
           'A1',
           'COMPRIDO',
           'CASTANHO',
-          true,
-          false,
+          1,
+          0,
           'Engenheiro',
           null
          );
 
 INSERT INTO servico (
-    id, nome, descricao, tempo_duracao, valor_servico, valor_sinal
+    nome, descricao, tempo_duracao, valor_servico, valor_sinal
 ) VALUES (
-             1,
           'Corte de Cabelo',
           'Corte estilo degradê com finalização',
           '01:30:00',
@@ -33,9 +31,8 @@ INSERT INTO servico (
          );
 
 INSERT INTO trancista (
-    id, nome, email, senha, token, tipo
+    nome, email, senha, token, tipo
 ) VALUES (
-             1,
           'Maria Silva',
           'maria.silva@exemplo.com',
           'senhaSegura123',
