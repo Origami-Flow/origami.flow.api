928cb4c17e7ceb5f66bffbcca5df1065e7146ae3
HeloSalgado <heloisa.salgado@sptech.school>
2024-11-22 22:27:32 -0300

    refatoração do data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,9 @@
 INSERT INTO cliente (
-    id, nome, email, senha,
+    nome, email, senha,
     data_nascimento, telefone, genero, tipo_cabelo,
     comprimento_cabelo, cor_cabelo, progressiva,
     primeira_tranca, ocupacao, endereco_id
 ) VALUES (
-             1,
              'João Silva',
              'joao.silva@example.com',
              'senha123',
@@ -14,16 +13,15 @@ INSERT INTO cliente (
              'A1',
              'COMPRIDO',
              'CASTANHO',
-             true,
-             false,
+             1,
+             0,
              'Engenheiro',
              null
          );
 
 INSERT INTO servico (
-    id, nome, descricao, tempo_duracao, valor_servico, valor_sinal
+    nome, descricao, tempo_duracao, valor_servico, valor_sinal
 ) VALUES (
-             1,
              'Corte de Cabelo',
              'Corte estilo degradê com finalização',
              '01:30:00',
@@ -32,11 +30,65 @@ INSERT INTO servico (
          );
 
 INSERT INTO trancista (
-    id, nome, email, senha, tipo
+    nome, email, senha, tipo
 ) VALUES (
-             1,
              'Maria Silva',
              'maria.silva@exemplo.com',
              'senhaSegura123',
              'Trancista'
+         );
+
+INSERT INTO endereco (
+    numero, bairro, cep, cidade, complemento, estado, logradouro, regiao, uf
+) VALUES (
+             518,
+             'Vila Ré',
+             '03658-000',
+             'São Paulo',
+             'A',
+             'São Paulo',
+             'Rua Itinguçu',
+             'Leste',
+             'SP'
+         );
+
+INSERT INTO salao (
+    nome, endereco_id
+) VALUES (
+             'Salgado Tranças',
+             1
+         );
+
+INSERT INTO produto (
+    quantidade_embalagem, valor_compra, valor_venda, marca, nome, tipo, unidade_medida
+) VALUES (
+             300,
+             20.9,
+             25.5,
+             'Salon Line',
+             'Pomada',
+            'LOJA',
+            'ml'
+         ),
+         (
+             500,
+             30.5,
+             0.0,
+             'Salon Line',
+             'Creme',
+             'SALAO',
+             'ml'
+         );
+
+INSERT INTO estoque (
+    produto_id, quantidade, salao_id
+) VALUES (
+             1,
+             10,
+             1
+         ),
+         (
+             2,
+             30,
+             1
          );
