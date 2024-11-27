c3a45192da8136d035b6299b6bf92564ab2fdf53
DanielRRSilva <daniel.rsilva@sptech.school>
2024-11-27 20:59:15 -0300

    feat/endpoints de criacao e atualizacao de auxiliar

--- src/main/java/origami_flow/salgado_trancas_api/mapper/AuxiliarMapper.java
+++ src/main/java/origami_flow/salgado_trancas_api/mapper/AuxiliarMapper.java
@@ -1,6 +1,8 @@
 package origami_flow.salgado_trancas_api.mapper;
 
 import org.mapstruct.Mapper;
+import origami_flow.salgado_trancas_api.dto.request.AuxiliarAtualizacaoRequestDTO;
+import origami_flow.salgado_trancas_api.dto.request.AuxiliarRequestDTO;
 import origami_flow.salgado_trancas_api.dto.response.AuxiliarDetalheResponseDTO;
 import origami_flow.salgado_trancas_api.entity.Auxiliar;
 
@@ -8,4 +10,8 @@ import origami_flow.salgado_trancas_api.entity.Auxiliar;
 public interface AuxiliarMapper {
 
     AuxiliarDetalheResponseDTO toDto(Auxiliar auxiliar);
+
+    Auxiliar toEntity(AuxiliarRequestDTO auxiliarRequestDTO);
+
+    Auxiliar toEntity(AuxiliarAtualizacaoRequestDTO auxiliarAtualizacaoRequestDTO);
 }
