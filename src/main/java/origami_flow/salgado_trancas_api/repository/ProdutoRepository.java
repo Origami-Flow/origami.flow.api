a8d80a09a7c0819b7f655c75d161ad5efe532367
davifeitosa0 <davi.rodrigues@sptech.school>
2024-11-30 00:52:32 -0300

    refactor: Mudando o endpoint de buscar por um nome especifico

--- src/main/java/origami_flow/salgado_trancas_api/repository/ProdutoRepository.java
+++ src/main/java/origami_flow/salgado_trancas_api/repository/ProdutoRepository.java
@@ -1,14 +1,17 @@
 package origami_flow.salgado_trancas_api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import origami_flow.salgado_trancas_api.entity.Produto;
-import origami_flow.salgado_trancas_api.utils.Lista;
 
 import java.util.List;
 
-public interface ProdutoRepository extends JpaRepository<Produto,Integer> {
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 
     boolean existsByNome(String nome);
 
-    List<Produto> findAllByOrderByNome();
+
+    @Query("select p from Produto p  where lower(p.nome) like concat('%',lower(:nome),'%')")
+    List<Produto> findAllByOrderByNome(String nome);
+
 }
